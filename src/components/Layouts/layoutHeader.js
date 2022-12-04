1e5f9722e6ed0063a919abff490894b154dfafb0
D:\учработа <savenko_andrey95@mail.ru>
2022-12-04 15:40:23 +0300

    fix header3

--- src/components/Layouts/layoutHeader.js
+++ src/components/Layouts/layoutHeader.js
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router-dom";
 import HeaderMain from "../allPages/headerMain/headerMainAll.js";
+import "../allPages/headerMain/headerMainAll.css"
 
 function LayoutHeader() {
     return (
