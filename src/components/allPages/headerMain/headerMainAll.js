4c2ffedd52ef151d4c218f0b933e8b29cd4b862c
D:\учработа <savenko_andrey95@mail.ru>
2022-12-04 15:16:09 +0300

    fix header

--- src/components/allPages/headerMain/headerMainAll.js
+++ src/components/allPages/headerMain/headerMainAll.js
@@ -3,12 +3,14 @@ import { Link } from 'react-router-dom';
 
 function HeaderMain() {
     return (
+        <>
         <div className='header-main__wrap'>
             <Link to='/'  className='header__logo'>night city shop</Link>
             <div className='header__menu'>
                 <Link to='/opinion' className='menu-link__reg'>Оставить отзыв</Link>
             </div>
         </div>
+        </>
     )
 
 }
